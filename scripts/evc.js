339d7dcab31f88f10566a68d47613213484f0df6
Prodigy-kun <ilerioluwaomotayo@gmail.com>
2023-08-29 06:29:35 +0100

    first commit

---
+++ scripts/evc.js
@@ -0,0 +1,9 @@
+document.querySelector(".ham").addEventListener("click", function () {
+  document.querySelector("nav").classList.toggle("nshow");
+  document.querySelector(".baseb").classList.toggle("nshow");
+  //   document.querySelector(".not").classList.toggle("nshow");
+});
+document.querySelector(".not").addEventListener("click", function () {
+  document.querySelector("nav").classList.toggle("nshow");
+  document.querySelector(".baseb").classList.toggle("nshow");
+});
